075a5efdf03dd60d2ec0d98628f4e01cd241dfc5
TidalJ <395478675@qq.com>
2024-06-01 21:00:55 +0930

    footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
-
+// import React, { useState } from 'react';
+import React from 'react';
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
-import { client } from '../../client';
+// import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-    const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-    const [loading, setLoading] = useState(false);
+    // const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+    // const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+    // const [loading, setLoading] = useState(false);
 
-    const { username, email, message } = formData;
+    // const { username, email, message } = formData;
 
-    const handleChangeInput = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+    // const handleChangeInput = (e) => {
+    //     const { name, value } = e.target;
+    //     setFormData({ ...formData, [name]: value });
+    // };
 
-    const handleSubmit = () => {
-        setLoading(true);
+    // const handleSubmit = () => {
+    //     setLoading(true);
 
-        const contact = {
-            _type: 'contact',
-            name: formData.username,
-            email: formData.email,
-            message: formData.message,
-        };
+    //     const contact = {
+    //         _type: 'contact',
+    //         name: formData.username,
+    //         email: formData.email,
+    //         message: formData.message,
+    //     };
 
-        client.create(contact)
-            .then(() => {
-                setLoading(false);
-                setIsFormSubmitted(true);
-            })
-            .catch((err) => console.log(err));
-    };
+    //     client.create(contact)
+    //         .then(() => {
+    //             setLoading(false);
+    //             setIsFormSubmitted(true);
+    //         })
+    //         .catch((err) => console.log(err));
+    // };
 
     return (
         <>
@@ -54,7 +54,7 @@ const Footer = () => {
                     </div>
                 </a>
             </div>
-            {!isFormSubmitted ? (
+            {/* {!isFormSubmitted ? (
                 <div className="app__footer-form app__flex">
                     <div className="app__flex">
                         <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
@@ -79,7 +79,7 @@ const Footer = () => {
                         Thank you for getting in touch!
                     </h3>
                 </div>
-            )}
+            )} */}
         </>
     );
 };
